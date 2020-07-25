a88a90fde9320ac86dd3c5892d8f488cd84fbe1f
Syed Muhammed Bilal <intergo878@gmail.com>
2020-07-25 11:01:34 +0500

    Add files via upload

--- src/TextField.tsx
+++ src/TextField.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 
 interface Person {
   firstName: string;
@@ -14,12 +14,23 @@ interface Props {
     f1: string
   }
   person: Person;
+  handleChange: (event: React.InputHTMLAttributes<HTMLInputElement>) => void;
 };
 
-const TextField: React.FC<Props> = () => {
+interface TextNode {
+  text: string;
+}
+
+const TextField: React.FC<Props> = ({handleChange}) => {
+
+  const [count, setCount] = useState<TextNode>({text: 'Hello'});
+  setCount({ text: 'hello' })
+  const inputRef = useRef<HTMLInputElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
+
   return (
-    <div>
-      <input />
+    <div ref={divRef}>
+      <input ref={inputRef} onChange={handleChange} />
     </div>
   )
 };
