05f2031acce38f9c610f5a3a5f3d39eba64cb512
Syed Muhammed Bilal <intergo878@gmail.com>
2020-07-25 11:17:29 +0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import TextField from './TextField';
+import { Counter } from './Counter';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <TextField text="hello" person={{firstName: '', lastName: ''}} />
+      <Counter>{
+        ({ count, setCount }) => (
+          <div>
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+          </div>
+        )
+      }</Counter>
     </div>
   );
 }
