fb0aedc505f05eeb58a5cab1e42e0b62caae7712
Syed Muhammed Bilal <intergo878@gmail.com>
2020-07-26 01:33:31 +0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import TextField from './TextField';
-import { Counter } from './Counter';
+
+type FormElem = React.FormEvent<HTMLFormElement>
+
+interface ITodo {
+  text: string;
+  completed: boolean;
+}
 
 const App: React.FC = () => {
+
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([])
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault()
+    setValue(' ')
+  }
+
   return (
     <div className="App">
-      <Counter>{
-        ({ count, setCount }) => (
-          <div>
-            {count}
-            <button onClick={() => setCount(count + 1)}>+</button>
-          </div>
-        )
-      }</Counter>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+      </form>
     </div>
   );
 }
